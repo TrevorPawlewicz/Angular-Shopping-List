ea5e7f75c92718f45a34301dfab551c0dcd66f1e
Trev <trevor.pawlewicz@gmail.com>
2017-01-27 10:40:33 -0500

    adding filters to ng-repeat list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('shoppingApp', ["ngRoute", "shoppingCtrlModule"]);
+
+var app = angular.module('shoppingApp', []);
+
+
+app.controller("HomeController", ["$scope", function($scope) {
+
+    $scope.appTitle = "Angular Shopping";
+}]);
+
+
+app.controller("GroceryListItemController", ["$scope", function($scope) {
+
+    $scope.groceryItems = [
+        {completed: true, itemName: 'milk', date: '2017-01-30'},
+        {completed: true, itemName: 'cheese', date: '2017-01-10'},
+        {completed: true, itemName: 'carrots', date: '2017-01-31'},
+        {completed: true, itemName: 'bread', date: '2017-01-29'}
+    ]
+}]);
